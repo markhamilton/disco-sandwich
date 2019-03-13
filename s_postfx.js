001716e8ba5de1c08fa187b9bf818b4119ac8b31
Mark Hamilton <mark@markhamilton.info>
2019-03-13 16:55:47 -0400

    Scaffold for post effects processor for sandwicher

---
+++ s_postfx.js
@@ -0,0 +1,49 @@
+class PostFX {
+
+    constructor(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        
+        this.__effects = [];
+    }
+    
+    addEffect(effect) {
+        this._effects.push(effect);
+    }
+    
+    render(ctx) {
+        this.__effects.forEach( (effect) => {
+            effect.render(ctx, this.x, this.y, this.w, this.h);
+        });
+    }
+}
+
+class Effect {
+    constructor(type) {
+        this.type = type;
+    }
+    
+    render(ctx, x, y, w, h) {
+        switch(this.type) {
+            case "hellfire":
+                // B hell T
+                break;
+            case "trippy":
+                break;
+            case "chromatic":
+                break;
+            case "blood":
+                break;
+            case "glitch":
+                break;
+            case "cursed":
+                break;
+            case "shimmer":
+                break;
+            case "scanlines":
+                break;
+        }
+    }
+}
